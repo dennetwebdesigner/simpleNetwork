bf3cf7e4387da776236c3a9a669eb0231219b0d6
Joatan Dennet <dennetwebdesigner@gmail.com>
2021-02-03 14:43:28 -0300

    alterações no layout da pagina cadastrar e melhorias no setRegister
    correções feita nos arquivos style.css e sign.css. Melhor ias feitas no arquivos ajax e sign js, e  correção de bug de validação de valores dos  inputs

--- src/frontend/assets/js/sign/Sign.js
+++ src/frontend/assets/js/sign/Sign.js
@@ -6,62 +6,60 @@ class Sign {
 
     }
 
+    // create new user
     register(form) {
         document.querySelector(form).addEventListener('submit', this.setRegister)
     }
 
-    async validateFields(e) {
-
-    }
-
     async setRegister(e) {
         e.preventDefault()
+
+        // return element message error
         const log = document.querySelector('#msg-log')
-        const input = e.target.elements
 
-        let data = {}
+        // validate fields
+        const firstname = e.target.elements.firstname
+        const lastanem = e.target.elements.lastanem
+        const birthday = e.target.elements.birthday
+        const username = e.target.elements.username
+        const email = e.target.elements.email
+        const password = e.target.elements.password
 
-        Object.keys(input).forEach((element, index) => {
-            const newElement = Object.values(input)[index]
-            if (
-                newElement.type != 'fieldset' &&
-                newElement.type != 'label' &&
-                newElement.type != 'submit'
-            ) {
-                if (!newElement.value) {
-                    this.validate = false
-                } else {
-                    this.validate = true
-                    data[newElement.name] = newElement.value
-                }
-            }
-        })
+        log.innerHTML = ''
 
-
-        if (!this.validate) {
+        // if inputs return false set validate in element message error
+        if (!firstname || !lastanem || !birthday || !username || !email || !password) {
             log.style.display = 'block'
             log.innerHTML = 'Não pode haver campos vazios!'
             return false
         }
 
+        // set connection with api
         const xhr = new Api()
         const api = await xhr.post('/users', data)
 
+        // case conecction success return status and set message in element  
         api.onreadystatechange = async() => {
-            if (xhr.status == 500) {
+
+            // case server not response
+            if (api.status == 500) {
                 log.style.display = 'block'
                 log.innerHTML = 'O servidor não esta respondendo, por favor tente novamente mais tarde!'
                 return false
             }
 
-            if (xhr.status == 404) {
+            // if email or username already register 
+            if (api.status == 404) {
                 log.style.display = 'block'
                 log.innerHTML = 'O email ou usuario já esta cadastrado!'
                 return false
             }
 
-            if (xhr == 200) {
-                alert('conta cadastrada com sucesso!')
+            // succesful new user create
+            if (api.status == 200) {
+                log.style.display = 'block'
+                log.style.background = 'rgba(30,150,0,0.6)'
+                log.innerHTML = 'Cadastrado com sucesso!'
                 return true
             }
 
