678a5edd9a187e5f0e831ed18ffa3fa06d268518
Joatan Dennet <dennetwebdesigner@gmail.com>
2021-02-06 17:19:01 -0300

    alteração nas notificações do arquivo sign
    junto com alterações de como a notificação aprecerá, arquivos, css e js foram alterados, class MessageLog foi criado pra tratar os textos

---
+++ src/frontend/assets/js/sign/MessagegLog.js
@@ -0,0 +1,25 @@
+class MessagegLog {
+
+    constructor() {
+        this.element = document.querySelector('#notification')
+    }
+
+    error(text) {
+        this.element.innerHTML = text
+        this.element.style.display = 'block'
+        this.element.style.color = 'red'
+        setTimeout(() => {
+            this.element.style.display = 'none'
+            this.element.style.color = 'black'
+        }, 1000 * 4)
+    }
+
+    success(text) {
+        this.element.innerHTML = text
+        this.element.style.display = 'block'
+        this.element.style.color = 'green'
+    }
+
+}
+
+export default new MessagegLog()
--- src/frontend/assets/js/sign/index.js
+++ src/frontend/assets/js/sign/index.js
@@ -1,4 +1,25 @@
 import sign from './Sign.js'
 
+
+const register = document.querySelector('#switch-register')
+const login = document.querySelector('#switch-login')
+
+const formLogin = document.querySelector('#login')
+const formRegister = document.querySelector('#register')
+
+login.addEventListener('click', () => {
+    login.classList.add('switch-form-active')
+    register.classList.remove('switch-form-active')
+    formLogin.style.display = 'block'
+    formRegister.style.display = 'none'
+})
+
+register.addEventListener('click', () => {
+    register.classList.add('switch-form-active')
+    login.classList.remove('switch-form-active')
+    formRegister.style.display = 'block'
+    formLogin.style.display = 'none'
+})
+
 sign.register('#register')
 sign.login('#login')
